acbe2761cdbd5f0850e70f99dc5f08660ef7a4c4
Joshen Lim <joshenlim@Joshens-MacBook-Pro.local>
2019-08-29 23:29:11 +0800

    Add PC client script to test connection between rpi and pc

---
+++ playgrounds/pc_client.py
@@ -0,0 +1,42 @@
+import socket
+import sys
+import time
+
+host = '192.168.3.1'
+port = 3053
+
+print('# Creating socket')
+try:
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+except socket.error:
+    print('Failed to create socket')
+    sys.exit()
+
+print('# Getting remote IP address') 
+try:
+    remote_ip = socket.gethostbyname(host)
+except socket.gaierror:
+    print('Hostname could not be resolved. Exiting')
+    sys.exit()
+
+# Connect to remote server
+print('# Connecting to server, ' + host + ' (' + remote_ip + ')')
+s.connect((remote_ip , port))
+
+# Send data to remote server
+while True:
+    time.sleep(5)
+    print('# Sending data to server')
+    msg = "Hello from yo\n"
+
+    try:
+        s.sendall(msg.encode('utf-8'))
+    except socket.error:
+        print ('Send failed')
+        sys.exit()
+
+# Receive data
+# print('# Receive data from server')
+# reply = s.recv(1024)
+
+# print('reply: ' + reply.strip().decode("UTF-8")) 
