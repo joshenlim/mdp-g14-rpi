6acf04d66838579946dccf12e998ed4b3d5f784c
MDP G14 RPI <mdp-g14-rpi@gmail.com>
2019-09-17 18:06:50 +0000

    Update android test playground script

--- playgrounds/android_test.py
+++ playgrounds/android_test.py
@@ -2,12 +2,20 @@ from src.communicator.Android import Android
 import time
 import json
 
+statuses = ['moving', 'stopped', 'turning', 'ready', 'completed']
+
+symbol_ids = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+
 android = Android()
 android.connect()
 
 while True:
-    # Send a status message
-    message = {'bot_status':'Moving'}
-    android.write(json.dumps(message))
-    time.sleep(1)
-    pass
+    for status in statuses:
+        message = {'bot_status' : status}
+        android.write(json.dumps(message))
+        time.sleep(1)
+        
+    for symbol_id in symbol_ids:
+        message = {'symbol_id' : symbol_id}
+        android.write(json.dumps(message))
+        time.sleep(1)
