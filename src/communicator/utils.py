9e8ba792561368cc1ea15fa937ae97de7d4dc395
Joshen Lim <joshenlim@Joshens-MacBook-Pro.local>
2019-09-30 16:14:05 +0800

    Update fpParser

--- src/communicator/utils.py
+++ src/communicator/utils.py
@@ -13,6 +13,9 @@ def fpParser(path_data):
     step_seq = []
 
     path_seq = path_data.split(";")
+    init_x, init_y, init_direction = path_seq[0][slice(1, len(path_seq[0]) - 1)].split(',')
+    print(init_x, init_y, init_direction)
+
     for index, path in enumerate(path_seq):
         x_1, y_1, direction_1 = path[slice(1, len(path) - 1)].split(',')
 
@@ -26,23 +29,23 @@ def fpParser(path_data):
                     step_count = abs(int(y_1) - int(y_2))
                 elif direction_1 in hori:
                     step_count = abs(int(x_1) - int(x_2))
-                step_seq = step_seq + [ 'F' for x in range(step_count) ]
+                step_seq = step_seq + [ 'f' for x in range(step_count) ]
             else:
                 if (direction_1 == 'N' and direction_2 =='E')\
                     or (direction_1 == 'S' and direction_2 == 'W')\
                     or (direction_1 == 'E' and direction_2 == 'S')\
                     or (direction_1 == 'W' and direction_2 == 'N'):
-                    step_seq.append('TR')
+                    step_seq.append('tr')
                 elif (direction_1 == 'N' and direction_2 =='W')\
                     or (direction_1 == 'S' and direction_2 == 'E')\
                     or (direction_1 == 'E' and direction_2 == 'N')\
                     or (direction_1 == 'W' and direction_2 == 'S'):
-                    step_seq.append('TL')
+                    step_seq.append('tl')
 
                 if direction_2 in vert:
                     step_count = abs(int(y_1) - int(y_2))
                 elif direction_2 in hori:
                     step_count = abs(int(x_1) - int(x_2))
-                step_seq = step_seq + [ 'F' for x in range(step_count) ]
+                step_seq = step_seq + [ 'f' for x in range(step_count) ]
 
-    return step_seq
+    return 'FP|{x}|{y}|{d}|{steps}'.format(x=init_x, y=init_y, d=init_direction, steps=','.join(step_seq))
